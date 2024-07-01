c83a418ebcd8c6fd2ac213ad0621497ca17143a0
kakhidzeaaN <kakhidzeaann@gmail.com>
2024-07-01 16:45:09 +0400

    first commit

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Topics(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    content = models.CharField(max_length=200)
+    group = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.group} _ {self.name}"
+
+
+# family: parenting, pregnancy, woman, man
+# Health: depression, chronic illness, anxiety
+# Other: addiction, sleep, grief and loss
--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Topics
+
 
 # Create your views here.
+def home(request):
+    # context = {"word": "Hello World!!!"}
+    topic = Topics.objects.all()
+    context = {"topics": topic}
+    return render(request, 'base/home.html', context)
+
+
+def about(request):
+    return render(request, 'base/about.html')
